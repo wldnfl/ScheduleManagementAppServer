705fe7eff2a00e8220eab1b52c22120f632d2e5a
jiwoo <1114jiwoo@gmail.com>
2024-05-17 00:30:49 +0900

    init commit

---
+++ src/main/java/com/sparta/schedulemanagementappserver/ScheduleManagementAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagementappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/schedulemanagementappserver/ScheduleManagementAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagementappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
